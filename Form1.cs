219d1a90771d8b8dee36f5848498620857dbdcee
Erdős Barnabás <83726220+Forry04@users.noreply.github.com>
2023-12-07 20:27:51 +0100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using TeliKep.subforms;
+
+namespace TeliKep
+{
+    public partial class Form1 : Form
+    {
+        private Random rnd = new Random();
+        private List<Point> snowflakes = new List<Point>();
+        bool snowing = true;
+        
+        
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.SetStyle(ControlStyles.UserPaint, true);
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            StartSnowfall();
+           
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            foreach (var snowflake in snowflakes)
+            {
+                e.Graphics.DrawString("*", new Font("Arial", 22), Brushes.White, snowflake);
+            }
+        }
+
+        async void StartSnowfall()
+        {
+            while (snowing)
+            {
+                snowflakes.Add(new Point(rnd.Next(0, this.Width), 0));
+
+
+                for (int i = 0; i < snowflakes.Count; i++)
+                {
+                    var snowflake = snowflakes[i];
+                    snowflakes[i] = new Point(snowflake.X, snowflake.Y + 1);
+
+                    if (snowflakes[i].Y > this.Height)
+                    {
+                        snowflakes.RemoveAt(i);
+                        i--;
+                    }
+                }
+
+                this.Invalidate();
+                await Task.Delay(100);
+            }
+        }
+
+        private void houseBtn_Click(object sender, EventArgs e)
+        {
+            snowing = false;
+            Inside inside = new Inside(this.Size);
+            inside.ShowDialog();
+            inside.player.Stop();
+            snowing = true;
+            StartSnowfall();
+        }
+
+       
+    }
+}
---
+++ subforms/Inside.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TeliKep.subforms
+{
+    public partial class Inside : Form
+    {
+        public SoundPlayer player = new SoundPlayer(@"../../../src/fire.wav");
+        public Inside(Size _size)
+        {
+            InitializeComponent();
+            player.Play();
+            this.Size =  _size;
+        }
+        
+    }
+}
